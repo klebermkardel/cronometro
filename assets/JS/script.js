47c2262cb99c156a90380356a55fb1263c64664c
klebermkardel <molinakardel@gmail.com>
2023-02-22 18:56:35 -0300

    Inicia a variável tempo em 0 e cria função para formatar o tempo

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,1 +1,12 @@
-console.log('Teste')
+let tempo = 0
+let cronometro
+
+function formatarTempo(tempo) {
+    let horas = Math.floor(tempo / 3600)
+    let minutos = Math.floor((tempo - (horas * 3600)) / 60)
+    let segundos = tempo - (horas * 3600) - (minutos * 60)
+    if (horas < 10) horas = "0" + horas
+    if (minutos < 10) minutos = "0" + minutos;
+    if (segundos < 10) segundos = "0" + segundos
+    return horas + ':' + minutos + ':' + segundos
+}
