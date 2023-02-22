3196f793e89e09b92c884c661509c18a295b347e
klebermkardel <molinakardel@gmail.com>
2023-02-22 19:53:52 -0300

    Cria eventos usando addEventListener e não usando mais o atributo no código html e corrige função de zerar cronômetro

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,28 +1,37 @@
-let tempo = 0
-let cronometro
+let tempo = 0;
+let cronometro;
 
 function formatarTempo(tempo) {
-    let horas = Math.floor(tempo / 3600)
-    let minutos = Math.floor((tempo - (horas * 3600)) / 60)
-    let segundos = tempo - (horas * 3600) - (minutos * 60)
-    if (horas < 10) horas = "0" + horas
-    if (minutos < 10) minutos = "0" + minutos;
-    if (segundos < 10) segundos = "0" + segundos
-    return horas + ':' + minutos + ':' + segundos
+  let horas = Math.floor(tempo / 3600);
+  let minutos = Math.floor((tempo - (horas * 3600)) / 60);
+  let segundos = tempo - (horas * 3600) - (minutos * 60);
+  if (horas < 10) horas = "0" + horas;
+  if (minutos < 10) minutos = "0" + minutos;
+  if (segundos < 10) segundos = "0" + segundos;
+  return horas + ':' + minutos + ':' + segundos;
 }
 
-function iniciarCronometro() {
-    cronometro = setInterval(() => {
-        tempo++
-        document.getElementById("cronometro").innerHTML = formatarTempo(tempo)
-    }, 1000)
+function cronometrar() {
+  if (cronometro) {
+    return; // retorna sem fazer nada se o cronômetro já estiver em execução
+  }
+  cronometro = setInterval(() => {
+    tempo++;
+    document.getElementById("cronometro").innerHTML = formatarTempo(tempo);
+  }, 1000);
 }
 
-function pararCronometro() {
-    clearInterval(cronometro)
+function parar() {
+  clearInterval(cronometro);
+  cronometro = null; // define cronômetro como null para que possa ser reiniciado
 }
 
-function zerarCronometro() {
-  tempo = 0
+function zerar() {
+  parar();
+  tempo = 0;
   document.getElementById("cronometro").innerHTML = formatarTempo(tempo);
 }
+
+document.getElementById("botao-iniciar").addEventListener("click", cronometrar);
+document.getElementById("botao-parar").addEventListener("click", parar);
+document.getElementById("botao-zerar").addEventListener("click", zerar);
