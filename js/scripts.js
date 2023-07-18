de142fd43121e5be192402296d5dc7663baddbfa
Kleber Molina Kardel <molinakardel@gmail.com>
2023-07-18 14:53:14 -0300

    Inicia variáveis de minutos, segundos e milisegundos em 0 e isPaused em false

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,8 @@ const startBtn = document.querySelector("startBtn")
 const pauseBtn = document.querySelector("pauseBtn")
 const resumeBtn = document.querySelector("resumeBtn")
 const resetBtn = document.querySelector("resetBtn")
+
+let minutes = 0
+let seconds = 0
+let milliseconds = 0
+let isPauses = false;
